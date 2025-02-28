58dd66bdeb6696a2d233d6ad2544a048afc44b17
raven-panda <ambroise.maire7@gmail.com>
2025-02-28 12:07:20 +0100

    add: increase coverage of bills container test and added GET integration test

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -5,11 +5,16 @@
 import {screen, waitFor} from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
-import { ROUTES_PATH} from "../constants/routes.js";
+import { ROUTES, ROUTES_PATH} from "../constants/routes.js";
 import {localStorageMock} from "../__mocks__/localStorage.js";
 import '@testing-library/jest-dom/extend-expect';
+import mockStore from "../__mocks__/store"
 
 import router from "../app/Router.js";
+import Bills from "../containers/Bills.js";
+import userEvent from "@testing-library/user-event";
+
+jest.mock("../app/store", () => mockStore);
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
@@ -36,4 +41,102 @@ describe("Given I am connected as an employee", () => {
       expect(dates).toEqual(datesSorted)
     })
   })
+
+  describe("When I am on Bills Page and I click on eye icons", () => {
+    test("Then file modal should appear", async () => {
+      document.body.innerHTML = BillsUI({ data: bills });
+
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      };
+
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }));
+
+      const billsContainer = new Bills({
+        document, onNavigate, store: null, localStorage: window.localStorage
+      });
+
+      const icon = screen.getAllByTestId("icon-eye")?.[0];
+      expect(icon).toBeTruthy();
+
+      const modal = await waitFor(() => screen.getByTestId("modaleFile"));
+      expect(modal).toBeTruthy();
+      
+      const handleShowModal = jest.fn((e) => billsContainer.handleClickIconEye(icon));
+      icon.addEventListener('click', handleShowModal);
+      userEvent.click(icon);
+      expect(handleShowModal).toHaveBeenCalled();   
+    })
+  });
 })
+
+// Integration test GET
+describe("Given I am user connected as an employee", () => {
+  describe("When I navigate to Bills", () => {
+    test("Then fetches bills from mock API GET", async () => {
+      localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }));
+      const root = document.createElement("div")
+      root.setAttribute("id", "root");
+      document.body.append(root);
+      router();
+      window.onNavigate(ROUTES_PATH.Bills);
+      await waitFor(() => screen.getByText("Mes notes de frais"));
+      
+      const tableContent = await screen.getByTestId("tbody");
+      expect(tableContent).toBeTruthy();
+    });
+  });
+  describe("When an error occurs on API", () => {
+    beforeEach(() => {
+      jest.spyOn(mockStore, "bills");
+      Object.defineProperty(
+        window,
+        'localStorage',
+        { value: localStorageMock }
+      )
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee',
+        email: "a@a"
+      }))
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.appendChild(root)
+      router()
+    });
+
+    test("Fetches bills from an API and fails with 404 message error", async () => {
+      mockStore.bills.mockImplementationOnce(() => {
+        return {
+          list: () => {
+            return Promise.reject(new Error("Erreur 404"))
+          }
+        }
+      });
+
+      window.onNavigate(ROUTES_PATH.Bills);
+      await new Promise(process.nextTick);
+      const message = await screen.getByText(/Erreur 404/);
+      expect(message).toBeTruthy();
+    });
+
+    test("Fetches bills from an API and fails with 500 message error", async () => {
+      mockStore.bills.mockImplementationOnce(() => {
+        return {
+          list: () => {
+            return Promise.reject(new Error("Erreur 500"))
+          }
+        }
+      });
+
+      window.onNavigate(ROUTES_PATH.Bills);
+      await new Promise(process.nextTick);
+      const message = await screen.getByText(/Erreur 500/);
+      expect(message).toBeTruthy();
+    })
+  })
+});
